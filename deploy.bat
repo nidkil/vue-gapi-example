60052a29c23c0ab4ab2c4b1cbdd84049ae9413ad
nidkil <info@nidkil.com>
2018-11-03 17:43:37 +0100

    Changed from gh-pages branch to master/docs deployment method

--- deploy.bat
+++ deploy.bat
@@ -2,11 +2,15 @@
 
 echo -------- Switch to branch: gh-pages --------
 
-git checkout gh-pages
+git checkout deploy
 
 echo -------- Build project --------
 
 npm run build
+
+echo -------- Changed directory from dist to docs --------
+
+mv dist docs
 
 echo -------- Add and commit build to branch: gh-pages --------
 
