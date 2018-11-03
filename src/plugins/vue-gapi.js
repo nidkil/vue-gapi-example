8ac742978fa075f2a1734ffb9a4296b3c12fe7c6
nidkil <info@nidkil.com>
2018-11-03 14:34:18 +0100

    Added error handling on initialization vue-gapi plugin

--- src/plugins/vue-gapi.js
+++ src/plugins/vue-gapi.js
@@ -1,10 +1,22 @@
 import Vue from "vue";
 import VueGAPI from "vue-gapi";
 
+const apiKey = process.env.VUE_APP_GOOGLE_API_KEY;
+const clientId = process.env.VUE_APP_GOOGLE_CLIENT_ID;
+
+if (!apiKey || !clientId) {
+  throw new Error(
+    "Has the .env.local file been setup? One or both variables are not set: VUE_APP_GOOGLE_API_KEY=" +
+      apiKey +
+      ", VUE_APP_GOOGLE_CLIENT_ID=" +
+      clientId
+  );
+}
+
 // See all available scopes here: https://developers.google.com/identity/protocols/googlescopes"
 const apiConfig = {
-  apiKey: process.env.VUE_APP_GOOGLE_API_KEY,
-  clientId: process.env.VUE_APP_GOOGLE_CLIENT_ID,
+  apiKey,
+  clientId,
   discoveryDocs: [
     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
   ],
